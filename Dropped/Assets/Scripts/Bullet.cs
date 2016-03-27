4713758b9d22a76ab262183dfaa571c531076a8e
JIH7 <JIHlucky7@gmail.com>
2016-03-27 17:47:07 -0500

    Added Bullet functionality/Animation
    Added gun and bullet Scripts. Animated bullets' muzzle flash. Gun
    instantiates bullets.

---
+++ Dropped/Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	public float bulletSpeed;
+
+	void Update () 
+	{
+		transform.position += bulletSpeed * Vector3.right * Time.deltaTime;
+	}
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Platforms")
+			Destroy (gameObject);
+	}
+}
---
+++ Dropped/Assets/Scripts/Gun.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+	
+	public GameObject bulletPrefab;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.X))
+			Shoot ();
+	}
+
+	void Shoot()
+	{
+		GameObject bullet = Instantiate (bulletPrefab, transform.position, transform.rotation) as GameObject;
+		Physics2D.IgnoreCollision (bullet.GetComponent<Collider2D> (), transform.parent.GetComponent<Collider2D> ());
+	}
+}
