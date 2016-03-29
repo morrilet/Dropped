3df999c87b05302f96f263ee84a1282cadf0be5c
JIH7 <JIHlucky7@gmail.com>
2016-03-29 16:45:53 -0500

    Polished Gun Script
    Temporarily removed muzzle flash as the animation was problematic.
    
    Added full auto mode to gun script toggled through a public bool.
    Added variable fire rate accessed through a public float (lower numbers
    = higher fire rate.)
    Added max range for bullets.

--- Dropped/Assets/Scripts/Bullet.cs
+++ Dropped/Assets/Scripts/Bullet.cs
@@ -4,10 +4,22 @@ using System.Collections;
 public class Bullet : MonoBehaviour {
 
 	public float bulletSpeed;
+	public float maxRange;
+	Vector3 startPos;
+
+	void Start()
+	{
+		startPos = transform.position;
+	}
 
 	void Update () 
 	{
 		transform.position += bulletSpeed * Vector3.right * Time.deltaTime;
+
+		if (maxRange <= startPos.x + transform.position.x)
+		{
+			Destroy (gameObject);
+		}
 	}
 	void OnCollisionEnter2D(Collision2D coll)
 	{
--- Dropped/Assets/Scripts/Gun.cs
+++ Dropped/Assets/Scripts/Gun.cs
@@ -2,14 +2,36 @@
 using System.Collections;
 
 public class Gun : MonoBehaviour {
-	
-	public GameObject bulletPrefab;
 
-	// Update is called once per frame
+	public bool isAuto; //True for full auto, false for semi auto.
+
+	public GameObject bulletPrefab; //Insert different bullet prefabs here for different guns
+	public float fireRate; //Fire rate of this gun (lower = faster!)
+	float fireRateCount;
+
+	void Start()
+	{
+		fireRateCount = fireRate;
+	}
+
 	void Update ()
 	{
-		if (Input.GetKeyDown (KeyCode.X))
-			Shoot ();
+		if (isAuto) { //Auto Fire
+			if (Input.GetKey (KeyCode.X) && fireRateCount >= fireRate) {
+				Shoot ();
+				fireRateCount = 0;
+			}
+		}
+		if (!isAuto) //Semi Auto Fire
+		{
+			if (Input.GetKeyDown (KeyCode.X) && fireRateCount >= fireRate)
+			{
+				Shoot ();
+				fireRateCount = 0;
+			}
+		}
+		fireRateCount += Time.deltaTime;
+		
 	}
 
 	void Shoot()
