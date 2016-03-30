1dd762be80198b6e8e703f43a4551e0ccdfec500
JIH7 <JIHlucky7@gmail.com>
2016-03-29 19:51:11 -0500

    More gunwork
    Modified bullet script so multiple bullets can be instantiated via
    Shoot(). Shoot() now required a float parameter that is passed along
    from a public float that can be modified in the inspector. Added bullet
    speed deviation for random variance in projectile speed.
    
    I used both of these new features to add a shotgun and (absurdly
    overpowered) flamethrower along with unique bullets for each.

--- Dropped/Assets/Scripts/Gun.cs
+++ Dropped/Assets/Scripts/Gun.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Gun : MonoBehaviour {
 
 	public bool isAuto; //True for full auto, false for semi auto.
-
+	public float bulletsPerShot;//Amount of bullets in a shot (for shotguns mostly)
 	public float rotationDeviation;//Innacuracy of gun 
 	public GameObject bulletPrefab; //Insert different bullet prefabs here for different guns
 	public float fireRate; //Fire rate of this gun (lower = faster!)
@@ -19,7 +19,7 @@ public class Gun : MonoBehaviour {
 	{
 		if (isAuto) { //Auto Fire
 			if (Input.GetKey (KeyCode.X) && fireRateCount >= fireRate) {
-				Shoot ();
+				Shoot (bulletsPerShot);
 				fireRateCount = 0;
 			}
 		}
@@ -27,7 +27,7 @@ public class Gun : MonoBehaviour {
 		{
 			if (Input.GetKeyDown (KeyCode.X) && fireRateCount >= fireRate)
 			{
-				Shoot ();
+				Shoot (bulletsPerShot);
 				fireRateCount = 0;
 			}
 		}
@@ -35,11 +35,14 @@ public class Gun : MonoBehaviour {
 		
 	}
 
-	void Shoot()
+	void Shoot(float bullets)
 	{
-		Quaternion rotationDeviationBuffer = new Quaternion ();
-		rotationDeviationBuffer.eulerAngles = new Vector3 (0, 0, Random.Range (-1 * rotationDeviation, rotationDeviation));
-		GameObject bullet = Instantiate (bulletPrefab, transform.position, transform.rotation * rotationDeviationBuffer) as GameObject;
-		Physics2D.IgnoreCollision (bullet.GetComponent<Collider2D> (), transform.parent.GetComponent<Collider2D> ());
+		for (float i = 0; i < bullets; i++)
+		{
+			Quaternion rotationDeviationBuffer = new Quaternion ();
+			rotationDeviationBuffer.eulerAngles = new Vector3 (0, 0, Random.Range (-1 * rotationDeviation, rotationDeviation));
+			GameObject bullet = Instantiate (bulletPrefab, transform.position, transform.rotation * rotationDeviationBuffer) as GameObject;
+			Physics2D.IgnoreCollision (bullet.GetComponent<Collider2D> (), transform.parent.GetComponent<Collider2D> ());
+		}
 	}
 }
