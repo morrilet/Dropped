871641c3800a3901960cec696009fbd031586f55
JIH7 <JIHlucky7@gmail.com>
2016-03-29 20:57:15 -0500

    Muzzle Flash Update
    Added a (really crappy)  muzzle flash.

--- Dropped/Assets/Scripts/Shooting/Gun.cs
+++ Dropped/Assets/Scripts/Shooting/Gun.cs
@@ -7,7 +7,15 @@ public class Gun : MonoBehaviour {
 	public float bulletsPerShot;//Amount of bullets in a shot (for shotguns mostly)
 	public float rotationDeviation;//Innacuracy of gun 
 	public GameObject bulletPrefab; //Insert different bullet prefabs here for different guns
+	public GameObject muzzleFlashPrefab;//Insert your preffered muzzle flash here
 	public float fireRate; //Fire rate of this gun (lower = faster!)
+
+	public float bulletOffSetX;//Controls origin point of bullet 
+	public float bulletOffSetY;
+
+	public float muzzleFlashOffSetX;//Controls origin point of muzzle flash
+	public float muzzleFlashOffSetY;
+
 	float fireRateCount;
 
 	void Start()
@@ -37,11 +45,14 @@ public class Gun : MonoBehaviour {
 
 	void Shoot(float bullets)
 	{
+		GameObject muzzleFlash = Instantiate (muzzleFlashPrefab, new Vector3(muzzleFlashOffSetX + transform.position.x, + muzzleFlashOffSetY + transform.position.y), transform.rotation) as GameObject;
+		muzzleFlash.transform.SetParent (this.transform);
+
 		for (float i = 0; i < bullets; i++)
 		{
 			Quaternion rotationDeviationBuffer = new Quaternion ();
 			rotationDeviationBuffer.eulerAngles = new Vector3 (0, 0, Random.Range (-1 * rotationDeviation, rotationDeviation));
-			GameObject bullet = Instantiate (bulletPrefab, transform.position, transform.rotation * rotationDeviationBuffer) as GameObject;
+			GameObject bullet = Instantiate (bulletPrefab, new Vector3(bulletOffSetX + transform.position.x, bulletOffSetY + transform.position.y), transform.rotation * rotationDeviationBuffer) as GameObject;
 			Physics2D.IgnoreCollision (bullet.GetComponent<Collider2D> (), transform.parent.GetComponent<Collider2D> ());
 		}
 	}
---
+++ Dropped/Assets/Scripts/Shooting/MuzzleFlash.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MuzzleFlash : MonoBehaviour 
+{
+	public float framesOfFlash;
+	float frameCounter;
+
+	void Start()
+	{
+		frameCounter = 0;
+	}
+
+	void Update ()
+	{
+		if (frameCounter >= framesOfFlash)
+			Destroy (gameObject);
+
+		frameCounter += Time.deltaTime;
+	}
+}
