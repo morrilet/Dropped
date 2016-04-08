bfd678bfdb51f05a83b88ef52857c96b47908175
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2016-04-07 20:41:09 -0700

    Fixed Doors... Again.
    https://pbs.twimg.com/media/CfdplraUMAA9CYg.jpg:large

--- Dropped/Assets/Scripts/Door.cs
+++ Dropped/Assets/Scripts/Door.cs
@@ -29,6 +29,7 @@ public class Door : MonoBehaviour
 
 	void Update()
 	{
+		Debug.Log(GetPlayerInsideDoor ());
 	}
 
 	//Returns whether the player is within the bounds of the door or not.
@@ -46,8 +47,8 @@ public class Door : MonoBehaviour
 		topLeftCorner     = RotatePoint (topLeftCorner);
 		topRightCorner    = RotatePoint (topRightCorner);
 
-		RaycastHit2D hitLeftSide  = Physics2D.Raycast (verts[1], (verts[2] - verts[1]).normalized, Vector2.Distance(transform.InverseTransformPoint(verts[2]), transform.InverseTransformPoint(verts[1])), mask);
-		RaycastHit2D hitRightSide = Physics2D.Raycast (verts[3], (verts[0] - verts[3]).normalized, Vector2.Distance(transform.InverseTransformPoint(verts[3]), transform.InverseTransformPoint(verts[0])), mask);
+		RaycastHit2D hitLeftSide = Physics2D.Linecast ((Vector2)verts[1], (Vector2)verts[2], mask);
+		RaycastHit2D hitRightSide = Physics2D.Linecast ((Vector2)verts [3], (Vector2)verts [0], mask);
 
 		if (!isOpen)
 			return false;
