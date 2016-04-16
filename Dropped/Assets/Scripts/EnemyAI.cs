585db47f7547c6d77ccd34f3abb5292c5cd2d0e8
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2016-04-15 22:00:06 -0700

    Began Work On An FSM For Enemies
    Introduced a new script (EnemyAI) and created a scene to start testing
    new AI features. EnemyAI uses the EnemyFSM along with several behaviour
    scripts to work. Currently in the process of porting over existing code
    to the new system.

---
+++ Dropped/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : Entity 
+{
+	[HideInInspector]
+	public Animator stateMachine;
+	[HideInInspector]
+	public EnemyInfo enemyInfo;
+
+	[HideInInspector]
+	public Controller2D controller;
+
+	[HideInInspector]
+	public Vector3 velocity;
+
+	public float gravity;
+	public float speed;
+
+	void Start()
+	{
+		stateMachine = GetComponent<Animator> ();
+		controller = GetComponent<Controller2D> ();
+	}
+
+	void Update()
+	{
+		base.Update ();
+
+		/*
+		if (controller.collisions.left && !controller.collisions.leftPrev)
+			enemyInfo.JustHitWall = true;
+		if (controller.collisions.right && !controller.collisions.rightPrev)
+			enemyInfo.JustHitWall = true;
+
+		if (!controller.collisions.belowLeft && controller.collisions.belowLeftPrev)
+			enemyInfo.IsOnEdgeOfPlatform = true;
+		if (!controller.collisions.belowRight && controller.collisions.belowRightPrev)
+			enemyInfo.IsOnEdgeOfPlatform = true;
+		*/
+
+		if (!controller.collisions.below)
+			velocity.y += gravity * Time.deltaTime;
+
+		//enemyInfo.Reset ();
+	}
+
+	public struct EnemyInfo
+	{
+		public bool JustHitWall;
+		public bool IsOnEdgeOfPlatform;
+
+		//Resets info.
+		public void Reset()
+		{
+			JustHitWall = false;
+			IsOnEdgeOfPlatform = false;
+		}
+	}
+}
---
+++ Dropped/Assets/StateMachines/Enemy/EnemyPatrolState.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPatrolState : StateMachineBehaviour 
+{
+	EnemyAI enemyAI;
+
+	Vector3 velocity;
+
+	// OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+	{
+		enemyAI = animator.gameObject.GetComponent<EnemyAI> ();
+
+		velocity = Vector3.zero;
+		velocity.x = enemyAI.speed;
+	}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) 
+	{
+		if (enemyAI.controller.collisions.left && !enemyAI.controller.collisions.leftPrev)
+			enemyAI.enemyInfo.JustHitWall = true;
+		if (enemyAI.controller.collisions.right && !enemyAI.controller.collisions.rightPrev)
+			enemyAI.enemyInfo.JustHitWall = true;
+
+		if (!enemyAI.controller.collisions.belowLeft && enemyAI.controller.collisions.belowLeftPrev)
+			enemyAI.enemyInfo.IsOnEdgeOfPlatform = true;
+		if (!enemyAI.controller.collisions.belowRight && enemyAI.controller.collisions.belowRightPrev)
+			enemyAI.enemyInfo.IsOnEdgeOfPlatform = true;
+		
+		enemyAI.controller.Move ((velocity + enemyAI.velocity) * Time.deltaTime);
+
+		if(enemyAI.enemyInfo.IsOnEdgeOfPlatform)
+			Debug.Log (enemyAI.enemyInfo.IsOnEdgeOfPlatform);
+
+		if (enemyAI.enemyInfo.IsOnEdgeOfPlatform || enemyAI.enemyInfo.JustHitWall) 
+		{
+			Debug.Log ("here");
+			velocity.x *= -1f;
+		}
+
+		enemyAI.enemyInfo.Reset ();
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
